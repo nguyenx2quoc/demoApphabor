b13fa1e53ae10ee8fc7310ae790515ddf312c405
nguyenx2quoc <nguyenx2quoc@gmail.com>
2016-11-25 14:10:22 +0700

    Xoá comment

--- DemoAppHabor/DemoAppHabor/App_Start/WebApiConfig.cs
+++ DemoAppHabor/DemoAppHabor/App_Start/WebApiConfig.cs
@@ -9,9 +9,7 @@ namespace DemoAppHabor
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
+            
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
